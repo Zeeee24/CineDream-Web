8bc4077094d2037f1540c448aa66630c3c2aebeb
Zaki <zaki@example.com>
2026-06-01 23:44:54 -0400

    Fix corrupted index.html and manifest.json from bad replaceAll

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-function stripCrossorigin() {
-  return {
-    name: 'strip-crossorigin',
-    transformIndexHtml(html) {
-      return html
-        .replace(/ crossorigin/g, '')
-    },
-  }
-}
-
 export default defineConfig({
-  plugins: [react(), stripCrossorigin()],
+  plugins: [react()],
   base: '/CineDream-Web/',
   build: {
     outDir: 'dist',
